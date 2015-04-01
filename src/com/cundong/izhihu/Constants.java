5b702cdb73216fea233bce9f47450868ddf3a9d0
cundong <cundong.liu@gmail.com>
2015-04-01 13:46:28 +0800

    use new api

--- src/com/cundong/izhihu/Constants.java
+++ src/com/cundong/izhihu/Constants.java
@@ -15,13 +15,13 @@ public final class Constants {
 	public static final class Url {
 
 		// 获取最新新闻
-		public static final String URL_LATEST = "http://news-at.zhihu.com/api/3/news/latest";
+		public static final String URL_LATEST = "http://news-at.zhihu.com/api/4/news/latest";
 
 		// 获取新闻详情
-		public static final String URL_DETAIL = "http://news-at.zhihu.com/api/3/news/";
+		public static final String URL_DETAIL = "http://news-at.zhihu.com/api/4/news/";
 
 		// 获取过往新闻
-		public static final String URLDEFORE = "http://news.at.zhihu.com/api/3/news/before/";
+		public static final String URLDEFORE = "http://news.at.zhihu.com/api/4/news/before/";
 	}
 	
 	public static final int NEWS_LIST = 1;
